bc5b4e2b53fea64936db58fa92de71d5dc8622eb
MrTrukiny <carloseduardo.tkn@gmail.com>
2023-03-21 12:26:28 -0500

    SE-11 feat(fizzbuzz): solve the fourth red-green-refactor case

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz/fizzbuzz.test.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz/fizzbuzz.test.ts
@@ -6,13 +6,27 @@ describe('Fizzbuzz', () => {
     expect(typeof result).toBe('string');
   });
 
-  it('Returns "Fizz" when the number is divisible by 3, such as 15', () => {
-    const result = fizzbuzz(15);
+  it('Returns "Fizz" when the number is divisible by 3, such as 6', () => {
+    const result = fizzbuzz(6);
     expect(result).toBe('Fizz');
   });
 
   it('Returns "Buzz" when the number is divisible by 5, such as 10', () => {
     const result = fizzbuzz(10);
     expect(result).toBe('Buzz');
+  });
+
+  it('Returns "FizzBuzz" when the number is divisible by 3 and 5, such as 15', () => {
+    const result = fizzbuzz(15);
+    expect(result).toBe('FizzBuzz');
+  });
+
+  it('Returns the number as a string when the number is not divisible by 3 or 5, such as 7', () => {
+    const result = fizzbuzz(7);
+    expect(result).toBe('7');
+  });
+
+  it('Throws an error when the number is lower than 1', () => {
+    expect(() => fizzbuzz(0)).toThrowError('Number must be greater than 0');
   });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz/fizzbuzz.ts
@@ -1,11 +1,10 @@
 export const fizzbuzz = (num: number) => {
+  if (num < 1) throw new Error('Number must be greater than 0');
   let result = num.toString();
 
-  if (num % 3 === 0) {
-    result = 'Fizz';
-  } else if (num % 5 === 0) {
-    result = 'Buzz';
-  }
+  if (num % 3 === 0) result = 'Fizz';
+  if (num % 5 === 0) result = 'Buzz';
+  if (num % 3 === 0 && num % 5 === 0) result = 'FizzBuzz';
 
   return result;
 };
